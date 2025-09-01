15bbc19b7529204183de81a4230a52466a9aa381
Angie C <a.cajusol98@gmail.com>
2025-09-01 16:08:20 +0200

    Footer structure

---
+++ src/components/sb/LinkGrid.js
@@ -0,0 +1,28 @@
+import { storyblokEditable } from "@storyblok/react/rsc";
+import Link from "./Link";
+
+export default function LinkGrid({ blok }) {
+  if (!blok || !blok.links) return null;
+  const direction =
+    blok.direction === "row"
+      ? "flex-row"
+      : "flex-col";
+  return (
+    <div
+      {...storyblokEditable(blok)}
+      className={`flex ${direction} gap-2`}
+    >
+      {blok.title && (
+        <div className="font-bold mb-2">
+          {blok.title}
+        </div>
+      )}
+      {blok.links.map((linkBlok, idx) => (
+        <Link
+          blok={linkBlok}
+          key={linkBlok._uid || idx}
+        />
+      ))}
+    </div>
+  );
+}
--- src/lib/storyblok.js
+++ src/lib/storyblok.js
@@ -1,30 +1,42 @@
-import { apiPlugin, storyblokInit } from '@storyblok/react/rsc';
+import {
+  apiPlugin,
+  storyblokInit,
+} from "@storyblok/react/rsc";
 
-import Page from '@/components/sb/Page';
-import Teaser from '@/components/sb/Teaser';
-import Feature from '@/components/sb/Feature';
-import Grid from '@/components/sb/Grid';
-import DoesNotExist from '@/components/sb/DoesNotExist';
-import Hero from '@/components/sb/Hero';
+import Page from "@/components/sb/Page";
+import Teaser from "@/components/sb/Teaser";
+import Feature from "@/components/sb/Feature";
+import Grid from "@/components/sb/Grid";
+import DoesNotExist from "@/components/sb/DoesNotExist";
+import Hero from "@/components/sb/Hero";
+import Link from "@/components/sb/Link";
+import Footer from "@/components/sb/Footer";
+
 export const components = {
-    // Add your components here
-    page: Page,
-    feature: Feature,
-    grid: Grid,
-    teaser: Teaser,
-    hero: Hero,
-    doesNotExist: DoesNotExist
-  };
+  // Add your components here
+  page: Page,
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  hero: Hero,
+  doesNotExist: DoesNotExist,
+  link: Link,
+  footer: Footer,
+};
 
-  /**
-   * Get the Storyblok API exports a StoryblokApi object to be used in the application
-   * @returns {StoryblokApi}
-   */
-  export const getStoryblokApi = storyblokInit({
-	accessToken: process.env.STORYBLOK_DELIVERY_API_ACCESS_TOKEN || process.env.NEXT_PUBLIC_STORYBLOK_DELIVERY_API_ACCESS_TOKEN,
-	use: [apiPlugin],
-	apiOptions: {
-		region: 'eu',
-	},
-    components
+/**
+ * Get the Storyblok API exports a StoryblokApi object to be used in the application
+ * @returns {StoryblokApi}
+ */
+export const getStoryblokApi = storyblokInit({
+  accessToken:
+    process.env
+      .STORYBLOK_DELIVERY_API_ACCESS_TOKEN ||
+    process.env
+      .NEXT_PUBLIC_STORYBLOK_DELIVERY_API_ACCESS_TOKEN,
+  use: [apiPlugin],
+  apiOptions: {
+    region: "eu",
+  },
+  components,
 });
