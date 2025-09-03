351eef33e7000de048b0b371d856c9e2b125b432
Angie C <a.cajusol98@gmail.com>
2025-09-03 19:32:57 +0200

    Pulled code from all branches into components

--- src/lib/storyblok.js
+++ src/lib/storyblok.js
@@ -1,44 +1,30 @@
-import {
-  apiPlugin,
-  storyblokInit,
-} from "@storyblok/react/rsc";
+import { apiPlugin, storyblokInit } from '@storyblok/react/rsc';
 
-import Page from "@/components/sb/Page";
-import Teaser from "@/components/sb/Teaser";
-import Feature from "@/components/sb/Feature";
-import Grid from "@/components/sb/Grid";
-import DoesNotExist from "@/components/sb/DoesNotExist";
-import Hero from "@/components/sb/Hero";
-import Link from "@/components/sb/Link";
-import Footer from "@/components/sb/Footer";
-import Header from "@/components/sb/Header";
-
+import Page from '@/components/sb/Page';
+import Teaser from '@/components/sb/Teaser';
+import Feature from '@/components/sb/Feature';
+import Grid from '@/components/sb/Grid';
+import DoesNotExist from '@/components/sb/DoesNotExist';
+import Hero from '@/components/sb/Hero';
 export const components = {
-  // Add your components here
-  page: Page,
-  feature: Feature,
-  grid: Grid,
-  teaser: Teaser,
-  hero: Hero,
-  doesNotExist: DoesNotExist,
-  link: Link,
-  footer: Footer,
-  header: Header,
-};
+    // Add your components here
+    page: Page,
+    feature: Feature,
+    grid: Grid,
+    teaser: Teaser,
+    hero: Hero,
+    doesNotExist: DoesNotExist
+  };
 
-/**
- * Get the Storyblok API exports a StoryblokApi object to be used in the application
- * @returns {StoryblokApi}
- */
-export const getStoryblokApi = storyblokInit({
-  accessToken:
-    process.env
-      .STORYBLOK_DELIVERY_API_ACCESS_TOKEN ||
-    process.env
-      .NEXT_PUBLIC_STORYBLOK_DELIVERY_API_ACCESS_TOKEN,
-  use: [apiPlugin],
-  apiOptions: {
-    region: "eu",
-  },
-  components,
+  /**
+   * Get the Storyblok API exports a StoryblokApi object to be used in the application
+   * @returns {StoryblokApi}
+   */
+  export const getStoryblokApi = storyblokInit({
+	accessToken: process.env.STORYBLOK_DELIVERY_API_ACCESS_TOKEN || process.env.NEXT_PUBLIC_STORYBLOK_DELIVERY_API_ACCESS_TOKEN,
+	use: [apiPlugin],
+	apiOptions: {
+		region: 'eu',
+	},
+    components
 });
