12a8277966808452b4b6d5652ae354cbc62dfc69
Angie C <a.cajusol98@gmail.com>
2025-09-03 21:59:38 +0200

    components

--- src/components/sb/Teaser.js
+++ src/components/sb/Teaser.js
@@ -1,10 +1,20 @@
 import { storyblokEditable } from "@storyblok/react";
 
-export default function Teaser({ blok }) {
-    return (
-        <div {...storyblokEditable(blok)} className="teaser p-4 rounded-md text-center">
-            <h2 className="text-2xl font-bold">{blok.headline}</h2>
-            <p className="text-gray-600">{blok.description}</p>
-        </div>
-    );
-    }
+export default function Teaser({
+  blok,
+  className = "",
+}) {
+  return (
+    <div
+      {...storyblokEditable(blok)}
+      className={`teaser p-4 rounded-md text-center ${className}`}
+    >
+      <h2 className="text-2xl font-bold">
+        {blok.headline}
+      </h2>
+      <p className="text-gray-600">
+        {blok.description}
+      </p>
+    </div>
+  );
+}
