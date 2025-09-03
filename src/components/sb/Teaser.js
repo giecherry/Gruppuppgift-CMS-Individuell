3905004616a33b316c0eab9538554a90196b0550
Angie C <a.cajusol98@gmail.com>
2025-09-03 21:24:27 +0200

    teaser text centered

--- src/components/sb/Teaser.js
+++ src/components/sb/Teaser.js
@@ -2,7 +2,7 @@ import { storyblokEditable } from "@storyblok/react";
 
 export default function Teaser({ blok }) {
     return (
-        <div {...storyblokEditable(blok)} className="teaser p-4 rounded-md">
+        <div {...storyblokEditable(blok)} className="teaser p-4 rounded-md text-center">
             <h2 className="text-2xl font-bold">{blok.headline}</h2>
             <p className="text-gray-600">{blok.description}</p>
         </div>
